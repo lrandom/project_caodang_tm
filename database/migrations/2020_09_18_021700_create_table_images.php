588293c8917bfc020f2f0c02d2255b800692e157
root <luann4099@gmail.com>
2020-09-18 09:19:56 +0700

    thêm bảng images

---
+++ database/migrations/2020_09_18_021700_create_table_images.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableImages extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('images', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('path', 200);
+            $table->integer('product_id', 200);
+            $table->tinyint('is_preview')->default(0);//0 0 phai anh dai dien, 1 la anh dd
+            $table->timestamp();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('images');
+    }
+}
