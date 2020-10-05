f3358ea3a531e59f26e20c13f977b92d95491a1a
root <luann4099@gmail.com>
2020-10-05 08:33:49 +0700

    thêm HomeController

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/welcome', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index');
 
 Route::prefix('admin')->group(function () {
     Route::get('/dashboard', 'DashboardController@index');
