717a98761aa0d22db24a176493b5246c07b26801
root <luann4099@gmail.com>
2020-10-30 11:06:36 +0700

    fix icon

--- resources/views/frontends/commons/head.blade.php
+++ resources/views/frontends/commons/head.blade.php
@@ -5,13 +5,14 @@
     <title> Shop </title>
     <!-- Place favicon.ico in the root directory -->
     <link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png">
-	<!-- font-awesome -->
-    <link href="{{asset('css/font-awesome.min.css')}}" rel="stylesheet">
-    <link href="{{asset('css/fonts.css')}}" rel="stylesheet">
-    <!-- Bootstrap -->
+    <!-- font-awesome -->
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+
+{{--    <link href="{{asset('css/fonts.css')}}" rel="stylesheet">--}}
+<!-- Bootstrap -->
     <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
-    <link rel="stylesheet" href="{{asset('css/owl.carousel.css')}}" />
-    <link rel="stylesheet" href="{{asset('css/owl.theme.default.css')}}" />
+    <link rel="stylesheet" href="{{asset('css/owl.carousel.css')}}"/>
+    <link rel="stylesheet" href="{{asset('css/owl.theme.default.css')}}"/>
     <!-- Animation Css -->
     <link href="{{asset('css/animate.css')}}" rel="stylesheet">
     <link href="{{asset('css/jquery-ui.min.css')}}" rel="stylesheet">
