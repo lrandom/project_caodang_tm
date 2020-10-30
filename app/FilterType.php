9599454776e47d3a94f8cdc6da144e7b8cab3e19
root <luann4099@gmail.com>
2020-10-30 12:03:53 +0700

    cập nhật menu

--- app/FilterType.php
+++ app/FilterType.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class FilterType extends Model
 {
     //
+    function filtersValue ()
+    {
+        return $this->hasMany('App\Category', 'filter_type_id', 'id');
+    }
 }
--- resources/views/frontends/commons/nav.blade.php
+++ resources/views/frontends/commons/nav.blade.php
@@ -1,5 +1,5 @@
 <?php
-$products = \Cart::session('cart')->getContent();
+use App\FilterType;$products = \Cart::session('cart')->getContent();
 $totalProduct = 0;
 foreach ($products as $r) {
     $totalProduct += $r->quantity;
@@ -9,7 +9,7 @@ foreach ($products as $r) {
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="container">
         <div class="navbar-header hidden-xs hidden-sm">
-            <a class="navbar-brand" href="index.html">
+            <a class="navbar-brand" href="{{url('/')}}">
                 <img src="{{asset('imgs/logo.png')}}" alt=""></a>
         </div>
         <!-- Collect the nav links, forms, and other content for toggling -->
@@ -20,144 +20,34 @@ foreach ($products as $r) {
                         Home
                     </a>
                 </li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
-                       aria-haspopup="true" aria-expanded="false">
-                        Pages
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li>
-                            <a href="#">
-                                About Us
-                                <span><i class="fa fa-caret-right"></i>
-													  </span>
-                            </a>
-                            <ul class="dropdown-menu-right">
-                                <li>
-                                    <a href="aboutus1.html"> About Style 01</a>
-                                </li>
-                                <li>
-                                    <a href="aboutus2.html"> About Style 02 </a>
-                                </li>
-                                <li>
-                                    <a href="aboutus3.html"> About Style 03 </a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a href="#">
-                                Services
-                                <span><i class="fa fa-caret-right"></i>
-													  </span>
-                            </a>
-                            <ul class="dropdown-menu-right">
-                                <li>
-                                    <a href="service_page1.html"> Services 01</a>
-                                </li>
-                                <li>
-                                    <a href="service_page2.html"> Services 02 </a>
-                                </li>
-                                <li>
-                                    <a href="service_page3.html"> Services 03</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a href="#">
-                                Team
-                                <span><i class="fa fa-caret-right"></i>
-													  </span>
-                            </a>
-                            <ul class="dropdown-menu-right">
-                                <li>
-                                    <a href="team_overlay_style.html"> Overly Style</a>
-                                </li>
-                                <li>
-                                    <a href="team_transparent_style.html"> Transparent Style</a>
-                                </li>
-                                <li>
-                                    <a href="team_circle_style.html"> Circle Style </a>
-                                </li>
-                                <li>
-                                    <a href="team_classic_style.html"> Classic Style </a>
-                                </li>
-                                <li>
-                                    <a href="team_creative_style.html"> Creative Style </a>
-                                </li>
-                                <li>
-                                    <a href="team_single.html"> Team Single </a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a href="faq.html">
-                                FAQ
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                Contact Us
-                                <span><i class="fa fa-caret-right"></i>
-													  </span>
-                            </a>
-                            <ul class="dropdown-menu-right">
-                                <li>
-                                    <a href="contact_us1.html"> Contact Us 01</a>
-                                </li>
-                                <li>
-                                    <a href="contact_us2.html"> Contact Us 02</a>
-                                </li>
-                                <li>
-                                    <a href="contact_us3.html"> Contact Us 03 </a>
-                                </li>
-                                <li>
-                                    <a href="contact_us4.html"> Contact Us 04 </a>
-                                </li>
-                                <li>
-                                    <a href="contact_us5.html"> Contact Us 05</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a href="#">
-                                Extra Pages
-                                <span><i class="fa fa-caret-right"></i>
-													  </span>
-                            </a>
-                            <ul class="dropdown-menu-right">
-                                <li>
-                                    <a href="404_style1.html"> 404 style 01</a>
-                                </li>
-                                <li>
-                                    <a href="404_style2.html"> 404 style 02</a>
-                                </li>
-                                <li>
-                                    <a href="404_style3.html"> 404 Animated </a>
-                                </li>
-                                <li>
-                                    <a href="404_style4.html"> 404 Full Width</a>
-                                </li>
-                                <li>
-                                    <a href="coming_soon_style1.html"> Coming Soon 01</a>
-                                </li>
-                                <li>
-                                    <a href="coming_soon_style2.html"> Coming Soon 02</a>
-                                </li>
-                                <li>
-                                    <a href="under_construction.html"> Under Construction </a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a href="login_and_register.html">
-                                Login/Register
-                            </a>
-                        </li>
 
-                    </ul>
-                </li>
+                <?php
+                $filterTypes = FilterType::with('filtersValue')->where('type', '1')->get();
+                ?>
+                @foreach($filterTypes as $r)
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                           aria-haspopup="true" aria-expanded="false">
+                            {{$r->name}}
+                        </a>
+                        <ul class="dropdown-menu">
+                            @foreach($r->filtersValue as $filterValue)
+                                <li>
+                                    <a href="#">
+                                        {{$filterValue->name}}
+                                        <span>
+                                            <i class="fa fa-caret-right"></i>
+                                        </span>
+                                    </a>
+                                </li>
+                            @endforeach
 
-                <!-- Cart Option -->
+                        </ul>
+                    </li>
+            @endforeach
+
+
+            <!-- Cart Option -->
                 <li class="dropdown cart_dropdown_wrapper">
                     <a href="{{url('cart')}}"><i
                             class="fa fa-shopping-cart"></i>
